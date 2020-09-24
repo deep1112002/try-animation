8340dc5e84f6d7779c5882083714945f95de14bb
deep1112002 <69797086+deep1112002@users.noreply.github.com>
2020-09-24 13:26:53 +0530

    Add files via upload

--- final.js
+++ final.js
@@ -1,19 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("nav-bar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
@@ -22,7 +6,6 @@ var scroll = window.requestAnimationFrame ||
 var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
 function loop() {
-
     Array.prototype.forEach.call(elementsToShow, function(element){
       if (isElementInViewport(element)) {
         element.classList.add('is-visible');
